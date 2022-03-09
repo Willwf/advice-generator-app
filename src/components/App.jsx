a0d42a386fbaf40d6beffe8b16fa05fafbf7c2be
William Firmino <williamwf07@gmail.com>
2022-03-09 04:33:26 -0300

    Using tailwind and creating html of the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div class="text-3xl font-bold underline">
+      <h1>Advice #117</h1>
+      <p>
+        "It is easy to sit up and take notice, what's difficult is getting up
+        and taking action."
+      </p>
+      <img src="./pattern-divider-mobile.svg" alt="Pattern Divider" />
+      <div>
+        <img src="./icon-dice.svg" alt="Icon Dice" />
+      </div>
     </div>
   );
 }
