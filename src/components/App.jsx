95567d24f89671418f59760f3773b4c6da9de45a
William Firmino <williamwf07@gmail.com>
2022-03-10 00:59:28 -0300

    Styled mobile design of the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 function App() {
   return (
-    <div class="text-3xl font-bold underline">
-      <h1>Advice #117</h1>
-      <p>
-        "It is easy to sit up and take notice, what's difficult is getting up
-        and taking action."
-      </p>
-      <img src="./pattern-divider-mobile.svg" alt="Pattern Divider" />
-      <div>
-        <img src="./icon-dice.svg" alt="Icon Dice" />
+    <main className="bg-dark-blue flex justify-center items-center h-screen w-screen">
+      <div className="relative font-bold bg-dark-greyish-blue w-[90vw] flex flex-col justify-center items-center rounded-lg">
+        <h1 className="uppercase text-center text-neon-green text-xs tracking-[.2rem] m-8">
+          Advice #117
+        </h1>
+        <p className="text-light-cyan text-center text-2xl w-11/12">
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
+        </p>
+        <img
+          className="mt-6 mb-14"
+          src="./pattern-divider-mobile.svg"
+          alt="Pattern Divider"
+        />
+        <button
+          type="submit"
+          className="absolute -bottom-8 bg-neon-green rounded-full h-16 w-16 flex justify-center items-center hover:shadow-dice-button active:opacity-70"
+        >
+          <img src="./icon-dice.svg" alt="Icon Dice" />
+        </button>
       </div>
-    </div>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "light-cyan": "hsl(193, 38%, 86%)",
+      "neon-green": "hsl(150, 100%, 66%)",
+      "greyish-blue": "hsl(217, 19%, 38%)",
+      "dark-greyish-blue": "hsl(217, 19%, 24%)",
+      "dark-blue": "hsl(218, 23%, 16%)",
+    },
+    fontFamily: {
+      sans: "Manrope, sans-serif",
+    },
+    extend: {
+      boxShadow: {
+        "dice-button": "0 0px 20px 6px hsl(150deg 100% 66% / 55%)",
+      },
+    },
   },
   plugins: [],
 };
