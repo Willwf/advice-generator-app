ab1a0deadf88435805c09685840556810a814c92
William Firmino <williamwf07@gmail.com>
2022-03-10 03:10:59 -0300

    adjustments for desktop layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,15 +17,15 @@ function App() {
 
   return (
     <main className="bg-dark-blue flex justify-center items-center h-screen w-screen">
-      <div className="relative font-bold bg-dark-greyish-blue w-[90vw] flex flex-col justify-center items-center rounded-lg">
+      <div className="lg:w-[33vw] w-[90vw] relative font-bold bg-dark-greyish-blue flex flex-col justify-center items-center rounded-lg">
         <h1 className="uppercase text-center text-neon-green text-xs tracking-[.2rem] m-8">
           Advice #{adviceData?.slip?.id}
         </h1>
-        <p className="text-light-cyan text-center text-2xl w-11/12">
-          {adviceData?.slip?.advice}
+        <p className="text-light-cyan text-center text-2xl w-10/12">
+          “{adviceData?.slip?.advice}”
         </p>
         <img
-          className="mt-6 mb-14"
+          className="mt-6 mb-14 lg:w-10/12 lg:content-[url('./pattern-divider-desktop.svg')]"
           src="./pattern-divider-mobile.svg"
           alt="Pattern Divider"
         />
